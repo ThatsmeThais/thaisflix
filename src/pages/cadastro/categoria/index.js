54012cc8058a72bec9292ec08939084c648c23f8
ThatsmeThais <thatarandrade25@gmail.com>
2020-08-01 00:02:24 -0300

    edit link heroku

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -32,7 +32,9 @@ function CadastroCategoria(){
 
       useEffect(() => {
         if(window.location.href.includes('localhost')) {
-          const URL = 'https://thaisflix.herokuapp.com/categorias'; 
+          const URL = window.location.hostname.includes('localhost') 
+            ?'http://localhost:8080/categorias'
+            :'https://thaisflix.herokuapp.com/categorias'; 
           fetch(URL)
            .then(async (respostaDoServer) =>{
             if(respostaDoServer.ok) {
